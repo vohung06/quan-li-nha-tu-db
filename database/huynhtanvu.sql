1b6f96fa2d4ead22e7af660ce135b692dfc881da
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-05-16 23:25:10 +0700

    Ghi những câu hỏi

--- database/huynhtanvu.sql
+++ database/huynhtanvu.sql
@@ -1,1 +1,3 @@
-đang tạo KHO
+--3.d.	Truy vấn lớn nhất, nhỏ nhất: 4 câu (4 đ) (Vũ)
+--3.f.	Truy vấn Hợp/Giao/Trừ: 3 câu (3đ) (Vũ)
+--4.	Tạo 7 thủ tục, 8 hàm và 5 trigger. Lưu ý thủ tục và hàm có cho ví dụ minh họa. (25 đ) 2 2 2 1
