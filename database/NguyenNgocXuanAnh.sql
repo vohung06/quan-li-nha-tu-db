0f8612f6a86b14fd307b74da51dcd901da451814
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-17 00:14:45 +0700

    tìm danh sách tù nhân theo giới tính

---
+++ database/NguyenNgocXuanAnh.sql
@@ -0,0 +1,9 @@
+﻿--Cau 4: Tìm danh sách tù nhân theo giới tính 
+CREATE PROC sp_gioitinh_select @GioiTinh NVARCHAR(5)
+AS BEGIN 
+SELECT MaTuNhan, SoCCCD, HoTen, GioiTinh
+FROM TUNHAN
+WHERE GioiTinh = @GioiTinh END;
+
+sp_gioitinh_select Nữ;
+sp_gioitinh_select Nam;
