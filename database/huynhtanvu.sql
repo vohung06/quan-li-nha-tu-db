d2c3c7b41e03f19c3d381d2a69a9ce6441cd3961
Huỳnh Tấn Vũ <vu201120066@gmail.com>
2026-05-16 23:26:40 +0700

    Tạo câu 1: Tù nhân lớn nhất

--- database/huynhtanvu.sql
+++ database/huynhtanvu.sql
@@ -1,3 +1,16 @@
 --3.d.	Truy vấn lớn nhất, nhỏ nhất: 4 câu (4 đ) (Vũ)
+--1. Tù nhân lớn tuổi nhất
+
+
+
+
+
+
+
+
+
+
+
+
 --3.f.	Truy vấn Hợp/Giao/Trừ: 3 câu (3đ) (Vũ)
 --4.	Tạo 7 thủ tục, 8 hàm và 5 trigger. Lưu ý thủ tục và hàm có cho ví dụ minh họa. (25 đ) 2 2 2 1
